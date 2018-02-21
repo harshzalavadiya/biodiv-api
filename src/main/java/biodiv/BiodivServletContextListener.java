20e0614c83b8ab2601860bd4335bc8c133587c87
pearlsravanthi <sravanthi@strandls.com>
2018-02-21 22:54:00 +0530

    Closing sessionFactory and deregistering jdbc driver on contextdestroy  for gradeful shutdown in prod mode and redeploy in dev mode

--- src/main/java/biodiv/BiodivServletContextListener.java
+++ src/main/java/biodiv/BiodivServletContextListener.java
@@ -2,17 +2,24 @@ package biodiv;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+
+import javax.servlet.ServletContextEvent;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
@@ -21,6 +28,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.c3p0.internal.C3P0ConnectionProvider;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.internal.SessionFactoryImpl;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.service.ServiceRegistry;
 import org.slf4j.Logger;
@@ -72,13 +82,61 @@ public class BiodivServletContextListener extends GuiceServletContextListener {
 
 				// Configuring hibernate session factory
 				try {
+					
+					   // Load the JDBC driver.
+				    
+				    
 					// loads configuration and mappings
 					org.hibernate.cfg.Configuration hibConf = new org.hibernate.cfg.Configuration();
+					
 					Properties dbProps = new Properties();
+					
 					dbProps.setProperty("hibernate.connection.url", config.getString("db.url"));
 					dbProps.setProperty("hibernate.connection.username", config.getString("db.username"));
 					dbProps.setProperty("hibernate.connection.password", config.getString("db.password"));
-					hibConf.configure("hibernate.cfg.xml").addProperties(dbProps);
+					dbProps.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+					
+					dbProps.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+					dbProps.setProperty("hibernate.dialect", "biodiv.common.MyPostgreSQL93Dialect");
+					dbProps.setProperty("hibernate.dialect", "org.hibernate.spatial.dialect.postgis.PostgisDialect");
+					
+					dbProps.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.EhCacheProvider");
+					
+					dbProps.setProperty("hibernate.c3p0.min_size", "5");
+					dbProps.setProperty("hibernate.c3p0.max_size", "20");
+					dbProps.setProperty("hibernate.c3p0.timeout", "300");
+					dbProps.setProperty("hibernate.c3p0.max_statements", "50");
+					dbProps.setProperty("hibernate.c3p0.idle_test_period", "3000");
+					dbProps.setProperty("hibernate.c3p0.testConnectionOnCheckout", "true");
+					dbProps.setProperty("hibernate.c3p0.preferredTestQuery", "SELECT 1");
+					dbProps.setProperty("hibernate.c3p0.numHelperThreads", "5");
+					dbProps.setProperty("hibernate.c3p0.unreturnedConnectionTimeout", "90");
+					dbProps.setProperty("hibernate.c3p0.maxConnectionAge", "1800");
+					dbProps.setProperty("hibernate.c3p0.debugUnreturnedConnectionStackTraces", "true");
+					//dbProps.setProperty("hibernate.c3p0.contextClassLoaderSource", "library");
+					//dbProps.setProperty("hibernate.c3p0.privilegeSpawnedThreads", "true");
+					
+					dbProps.setProperty("hbm2ddl.auto", "update");
+					
+					dbProps.setProperty("hibernate.id.new_generator_mappings", "false");
+					dbProps.setProperty("hibernate.transaction.coordinator_class", "jdbc");
+					dbProps.setProperty("hibernate.current_session_context_class", "thread");
+					dbProps.setProperty("hibernate.jdbc.batch_size", "50");
+					
+					dbProps.setProperty("show_sql", "true");
+				/*
+					
+			
+					
+					ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+					InputStream input = classLoader.getResourceAsStream("hibernate.cfg.xml");
+					//Properties properties = new Properties();
+					//properties.load(input);
+					//URL configFileURL = getClass().getClassLoader().getResource("/hibernate.cfg.xml");
+					//log.debug("Using hibernate configuration file : {}", configFileURL);
+					
+					dbProps.load(input);*/
+					hibConf.addProperties(dbProps);
 					for (Class cls : getEntityClassesFromPackage("biodiv")) {
 						log.trace("Adding annotated class : {}", cls);
 						hibConf.addAnnotatedClass(cls);
@@ -163,6 +221,49 @@ public class BiodivServletContextListener extends GuiceServletContextListener {
 		});
 
 		return names;
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		Injector injector = (Injector) sce.getServletContext()
+                 .getAttribute(Injector.class.getName()); 
+		SessionFactory sessionFactory = injector.getInstance(SessionFactory.class);
+/*		if(sessionFactory instanceof SessionFactoryImpl) {
+		      SessionFactoryImpl sf = (SessionFactoryImpl)sessionFactory;
+		      ConnectionProvider conn = sf.getConnectionProvider();
+		      if(conn instanceof C3P0ConnectionProvider) { 
+		        ((C3P0ConnectionProvider)conn).stop(); 
+		      }
+		   }
+*/	
+		sessionFactory.close();
+
+		 
+		super.contextDestroyed(sce);
+		// ... First close any background tasks which may be using the DB ...
+	    // ... Then close any DB connection pools ...
+
+	    // Now deregister JDBC drivers in this context's ClassLoader:
+	    // Get the webapp's ClassLoader
+	    ClassLoader cl = Thread.currentThread().getContextClassLoader();
+	    // Loop through all drivers
+	    Enumeration<Driver> drivers = DriverManager.getDrivers();
+	    while (drivers.hasMoreElements()) {
+	        Driver driver = drivers.nextElement();
+	        if (driver.getClass().getClassLoader() == cl) {
+	            // This driver was registered by the webapp's ClassLoader, so deregister it:
+	            try {
+	                log.info("Deregistering JDBC driver {}", driver);
+	                DriverManager.deregisterDriver(driver);
+	            } catch (SQLException ex) {
+	                log.error("Error deregistering JDBC driver {}", driver, ex);
+	            }
+	        } else {
+	            // driver was not registered by the webapp's ClassLoader and may be in use elsewhere
+	            log.trace("Not deregistering JDBC driver {} as it does not belong to this webapp's ClassLoader", driver);
+	        }
+	    }
+	    
 	}
 
 }
