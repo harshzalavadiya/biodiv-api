b798acd8edc8653eb0b22bc629e7f549f8356790
mukund <mukund@strandls.com>
2018-05-17 13:22:22 +0530

    Adding write to csv methods

---
+++ src/main/java/biodiv/scheduler/CSVUtils.java
@@ -0,0 +1,146 @@
+package biodiv.scheduler;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CSVUtils {
+
+    private static final char DEFAULT_SEPARATOR = ',';
+    private static final char DEFAULT_QUOTE = '"';
+
+    public static List<String> parseLine(String cvsLine) {
+        return parseLine(cvsLine, DEFAULT_SEPARATOR, DEFAULT_QUOTE);
+    }
+
+    public static List<String> parseLine(String cvsLine, char separators) {
+        return parseLine(cvsLine, separators, DEFAULT_QUOTE);
+    }
+
+    public static List<String> parseLine(String cvsLine, char separators, char customQuote) {
+
+        List<String> result = new ArrayList<>();
+
+        //if empty, return!
+        if (cvsLine == null || cvsLine.isEmpty()) {
+            return result;
+        }
+
+        if (customQuote == ' ') {
+            customQuote = DEFAULT_QUOTE;
+        }
+
+        if (separators == ' ') {
+            separators = DEFAULT_SEPARATOR;
+        }
+
+        StringBuilder curVal = new StringBuilder();
+        boolean inQuotes = false;
+        boolean startCollectChar = false;
+        boolean doubleQuotesInColumn = false;
+
+        char[] chars = cvsLine.toCharArray();
+
+        for (char ch : chars) {
+
+            if (inQuotes) {
+                startCollectChar = true;
+                if (ch == customQuote) {
+                    inQuotes = false;
+                    doubleQuotesInColumn = false;
+                } else {
+
+                    //Fixed : allow "" in custom quote enclosed
+                    if (ch == '\"') {
+                        if (!doubleQuotesInColumn) {
+                            curVal.append(ch);
+                            doubleQuotesInColumn = true;
+                        }
+                    } else {
+                        curVal.append(ch);
+                    }
+
+                }
+            } else {
+                if (ch == customQuote) {
+
+                    inQuotes = true;
+
+                    //Fixed : allow "" in empty quote enclosed
+                    if (chars[0] != '"' && customQuote == '\"') {
+                        curVal.append('"');
+                    }
+
+                    //double quotes in column will hit this!
+                    if (startCollectChar) {
+                        curVal.append('"');
+                    }
+
+                } else if (ch == separators) {
+
+                    result.add(curVal.toString());
+
+                    curVal = new StringBuilder();
+                    startCollectChar = false;
+
+                } else if (ch == '\r') {
+                    //ignore LF characters
+                    continue;
+                } else if (ch == '\n') {
+                    //the end, break!
+                    break;
+                } else {
+                    curVal.append(ch);
+                }
+            }
+
+        }
+
+        result.add(curVal.toString());
+
+        return result;
+    }
+
+	private static String followCVSformat(Object value) {
+		if (value == null)
+			return "";
+
+		String result = value.toString();
+		result = result.replace("\n", "").replace("\r", "");
+		// https://tools.ietf.org/html/rfc4180
+		if (result.contains("\"")) {
+			result = result.replace("\"", "\"\"");
+		}
+		if (result.contains(",")) {
+			result = "\"" + result + "\"";
+		}
+		return result;
+	}
+
+	public static String getCsvString(Collection<Object> values) {
+		boolean first = true;
+
+		StringBuilder sb = new StringBuilder();
+		for (Object value : values) {
+			if (!first)
+				sb.append(DEFAULT_SEPARATOR);
+
+			sb.append(followCVSformat(value));
+			first = false;
+		}
+
+		sb.append("\n");
+		return sb.toString();
+	}
+
+	public static byte[] getCsvBytes(Collection<Object> values) {
+		return getCsvString(values).getBytes();
+	}
+
+	public static void writeCsvLine(Writer w, Collection<Object> values) throws IOException {
+		w.write(getCsvString(values));
+	}
+
+}
