05d713f6f79dfa49c0aad841bc68d67a246b67f1
mukund <mukund@strandls.com>
2018-06-25 11:39:42 +0530

    Adding terms aggr related changes

--- src/main/java/biodiv/common/NakshaUrlService.java
+++ src/main/java/biodiv/common/NakshaUrlService.java
@@ -12,4 +12,20 @@ public class NakshaUrlService {
 	public String getDownloadUrl(String index, String type, String geoField) {
 		return config.getString("naksha.url") + "/services/download/" + index + "/" + type + "?filePath=" + config.getString("download.filePath") + "&geoField=" + geoField;
 	}
+	
+	public String getTermsAggregationUrl(String index, String type, String field, String geoField, Integer size) {
+		StringBuilder builder = new StringBuilder();
+		builder.append(config.getString("naksha.url"));
+		builder.append("/services/terms-aggregation/");
+		builder.append(index);
+		builder.append("/");
+		builder.append(type);
+		builder.append("?field=");
+		builder.append(field);
+		builder.append("&size=");
+		builder.append(size);
+		builder.append("&locationField=");
+		builder.append(geoField);
+		return builder.toString();
+	}
 }
